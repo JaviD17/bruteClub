815dc1436fd8dcabce783f6f9f5f2866f1f37ea5
Javier Dominguez II <Javi7057@gmail.com>
2022-04-16 13:08:56 -0500

    card component composition img tag finished, align text and rest of card

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -11,9 +11,10 @@
 </script>
 
 <section id="card" class="w-full h-full p-8">
-	<div id="inner-card" class="bg-black/40 p-px rounded-lg text-center">
+	<div id="inner-card" class="bg-black/25 p-px rounded-lg text-center h-48 w-48">
 		{#if $$slots.info}
 			{#each newBlog as { title, date, author }}
+				<div class="p-4"><img src="./blog1.jpg" alt="man prepping food" class="rounded-lg h-36 object-cover object-right-bottom" id="card-img"/></div>
 				<h2 class="m-4">{title}</h2>
 				<slot name="info" {date} />
 				<p class="m-4">{author}</p>
@@ -25,7 +26,10 @@
 <style>
 	#inner-card {
 		/* background-color: linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)); */
-        height: 24rem;
-        width: 35rem;
+		height: 24rem;
+		width: 35rem;
 	}
+    #card-img {
+        width: 34rem;
+    }
 </style>
