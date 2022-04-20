a9aef06b857a4ee96256043584be07d9f42e9f13
Javier Dominguez II <Javi7057@gmail.com>
2022-04-20 17:26:49 -0500

    calculator layout adjusted with flexbox and formulas finished for men but womens needs to be adjusted

--- src/lib/components/calcCard.svelte
+++ src/lib/components/calcCard.svelte
@@ -2,178 +2,210 @@
 	import Button from '$lib/components/button.svelte';
 
 	let macros = {
-		protein: '',
-		carbs: '',
-		fat: ''
+		protein: null,
+		carbs: null,
+		fat: null
 	};
+
 	// All Calcs
 	// Macro Calc
 	// FFMI Calc
 	// 1RM Calc
 	// Body fat chart
 
-	// conversion calculators
-
-	// lbs to kg
-	// ft to cm
-
-	// macro calculator
-
-	// input for gender
-	// input for weight (lbs)
-	// height ft
-	// height in
-	// age
-
-	// let gender;
-	// let weight = '';
-	// let fHeight = '';
-	// let iHeight = '';
-	// let age = '';
-
-	// let calcData = [
-	let macro = {
+	let calcData = {
 		name: 'Macro Calculator',
 		gender: '',
 		weight: null,
 		heightFt: null,
 		heightIn: null,
 		age: null,
-		activity: '',
-		goal: ''
+		activity: null,
+		goal: null
 	};
-	// { name: 'Macro Calculator', gender: '', weight: '', heightFt: '', heightIn: '', age: '' },
-	// ];
 
 	function formulaM() {
-		let weightKg = macro.weight * 0.453592;
-		let heightCm = macro.heightFt * 30.48 + macro.heightIn * 2.54;
-		let formulaM =
-			10 * weightKg +
-			6.25 * heightCm -
-			5 * macro.age +
-			5;
+		let weightKg = calcData.weight * 0.453592;
+		let heightCm = calcData.heightFt * 30.48 + calcData.heightIn * 2.54;
+		let formulaM = 10 * weightKg + 6.25 * heightCm - 5 * calcData.age + 5;
+		formulaM = formulaM * calcData.activity;
+		formulaM = formulaM * calcData.goal;
 
-		return formulaM;
+		if (calcData.goal === '1') {
+			return {
+				protein: Math.floor((formulaM * 0.3) / 4),
+				carbs: Math.floor((formulaM * 0.4) / 4),
+				fat: Math.floor((formulaM * 0.3) / 9)
+			};
+		} else if (calcData.goal === '1.1' || calcData.goal === '1.2' || calcData.goal === '1.4') {
+			return {
+				protein: Math.floor((formulaM * 0.3) / 4),
+				carbs: Math.floor((formulaM * 0.5) / 4),
+				fat: Math.floor((formulaM * 0.2) / 9)
+			};
+		} else if (calcData.goal === '0.9' || calcData.goal === '0.8' || calcData.goal === '0.6') {
+			return {
+				protein: Math.floor((formulaM * 0.5) / 4),
+				carbs: Math.floor((formulaM * 0.25) / 4),
+				fat: Math.floor((formulaM * 0.25) / 9)
+			};
+		}
+	}
+	function formulaW() {
+		let weightKg = calcData.weight * 0.453592;
+		let heightCm = calcData.heightFt * 30.48 + calcData.heightIn * 2.54;
+		let formulaW = 10 * weightKg + 6.25 * heightCm - 5 * calcData.age - 161;
+		formulaW = formulaW * calcData.activity;
+		formulaW = formulaW * calcData.goal;
+
+		if (calcData.goal === '1') {
+			return {
+				protein: Math.floor((formulaW * 0.3) / 4),
+				carbs: Math.floor((formulaW * 0.4) / 4),
+				fat: Math.floor((formulaW * 0.3) / 9)
+			};
+		} else if (calcData.goal === '1.1' || calcData.goal === '1.2' || calcData.goal === '1.4') {
+			return {
+				protein: Math.floor((formulaW * 0.3) / 4),
+				carbs: Math.floor((formulaW * 0.5) / 4),
+				fat: Math.floor((formulaW * 0.2) / 9)
+			};
+		} else if (calcData.goal === '0.9' || calcData.goal === '0.8' || calcData.goal === '0.6') {
+			return {
+				protein: Math.floor((formulaW * 0.5) / 4),
+				carbs: Math.floor((formulaW * 0.25) / 4),
+				fat: Math.floor((formulaW * 0.25) / 9)
+			};
+		}
 	}
 
 	function calculate() {
-		console.log({ macro });
-		macros.protein = '220' + 'P';
-		macros.carbs = '300' + 'C';
-		macros.fat = '60' + 'F';
-		console.log({ macros });
+		console.log({ calcData });
+		if (calcData.gender == 'male') {
+			let { protein, carbs, fat } = formulaM();
+			// console.log(protein, carbs, fat);
+			macros.protein = protein;
+			macros.carbs = carbs;
+			macros.fat = fat;
+			console.log({ macros });
+		} else if (calcData.gender === 'female') {
+			let { protein, carbs, fat } = formulaW();
+			macros.protein = protein;
+			macros.carbs = carbs;
+			macros.fat = fat;
+			console.log({ macros });
+		}
 
-		let answer = formulaM();
-		console.log(answer);
+		// maintain ratios (balanced)
+		// 30% P, 40% C, 30% F
 
-		// 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5
+		// cut ratios (high protein)
+		// 50% P, 25% C, 25% F
+
+		// bulk ratios (high carb)
+		// 30% P, 50% C, 20% F
 	}
 </script>
 
 <section
-	class="grid bg-black/50 mx-10 my-8 pb-2 rounded-lg outline outline-offset-2 outline-cyan-300"
+	class="grid bg-black/80 mx-10 my-8 pb-2 rounded-lg outline outline-offset-2 outline-cyan-300"
 >
-	<h3 class="text-center mt-4 text-2xl font-bold">{macro.name}</h3>
+	<h3 class="text-center mt-4 text-2xl font-bold">{calcData.name}</h3>
 	<form id="form" class="text-xl justify-items-center pt-4">
 		<div class="div-calc">
-			<label for="gender">Gender</label><br />
-			<select bind:value={macro.gender} class="input-btn" id="gender" name="gender">
+			<label for="gender" class="label-btn">Gender</label><br />
+			<select bind:value={calcData.gender} class="input-btn" id="gender" name="gender">
+				<option value="" />
 				<option value="male">Male</option>
 				<option value="female">Female</option>
 			</select>
 		</div>
 		<div class="div-calc">
-			<label for="weight">Weight (lbs.)</label><br />
+			<label for="weight" class="label-btn">Weight (lbs.)</label><br />
 			<input
 				class="input-btn"
 				size="20"
 				type="text"
 				id="weight"
 				name="weight"
-				bind:value={macro.weight}
+				bind:value={calcData.weight}
 				placeholder="Enter Weight (lbs)"
 			/>
 		</div>
 
 		<div class="div-calc">
-			<label for="heightF">Height (ft.)</label><br />
+			<label for="heightF" class="label-btn">Height (ft.)</label><br />
 			<input
 				class="input-btn"
 				type="text"
 				id="heightF"
 				name="heightF"
-				bind:value={macro.heightFt}
+				bind:value={calcData.heightFt}
 				placeholder="Enter Height (ft.)"
 			/>
 		</div>
 		<div class="div-calc">
-			<label for="heightI">Height (in.)</label><br />
+			<label for="heightI" class="label-btn">Height (in.)</label><br />
 			<input
 				class="input-btn"
 				type="text"
 				id="heightI"
 				name="heightI"
-				bind:value={macro.heightIn}
+				bind:value={calcData.heightIn}
 				placeholder="Enter Height (in.)"
 			/>
 		</div>
 		<div class="div-calc">
-			<label for="age">Age</label><br />
+			<label for="age" class="label-btn">Age</label><br />
 			<input
 				class="input-btn"
 				type="text"
 				id="age"
 				name="age"
-				bind:value={macro.age}
+				bind:value={calcData.age}
 				placeholder="Enter Age"
 			/>
 		</div>
-		<div class="div-calc">
-			<label for="activity">Activity</label><br />
-			<select class="input-btn" id="activity" name="activity" bind:value={macro.activity}>
-				<option value="sedentary">Sedentary (little to no exercise in a day)</option>
-				<option value="slightly">Slightly Active (light exercise/sports 1-3 days/week)</option>
-				<option value="moderately"
-					>Moderately Active (moderate exercise/sports 3-5 days/week)</option
-				>
-				<option value="very">Very Active (hard exercise/sports 6-7 days a week)</option>
-				<option value="extra"
-					>Extra Active (very hard exercise/sports and physical job OR 2x training)</option
-				>
+		<div class="">
+			<label for="activity" class="label-btn">Activity</label><br />
+			<select class="input-btn" id="activity" name="activity" bind:value={calcData.activity}>
+				<option value="1.15">Sedentary (little to no exercise)</option>
+				<option value="1.3">Slightly Active (exercise/sports 1-3 hrs/week)</option>
+				<option value="1.50">Moderately Active (exercise/sports 4-6 hrs/week)</option>
+				<option value="1.7">Very Active (exercise/sports 7-9 hrs/week)</option>
+				<option value="1.9">Extra Active (exercise/sports 10+ hrs/week)</option>
 			</select>
 		</div>
-		<div class="div-calc">
-			<label for="goal">Goal</label><br />
-			<select class="input-btn" id="goal" name="goal" bind:value={macro.goal}>
-				<option value="rwl">Rapid Weight Loss (~2lb/week)</option>
-				<option value="mwl">Moderate Weight Loss (~lb/week)</option>
-				<option value="swl">Slow Weight Loss (~0.5lb/week)</option>
-				<option value="mw">Maintain Weight (0lb/week)</option>
-				<option value="slg">Slow Weight Gain (0.5% body weight/month)</option>
-				<option value="mwg">Moderate Weight Gain (1% body weight/month)</option>
-				<option value="rwg">Rapid Weight Gain (2% body weight/month)</option>
+		<div class="">
+			<label for="goal" class="label-btn">Goal</label><br />
+			<select class="input-btn" id="goal" name="goal" bind:value={calcData.goal}>
+				<option value="0.6">Rapid Weight Loss (~2lb/week)</option>
+				<option value="0.8">Moderate Weight Loss (~lb/week)</option>
+				<option value="0.9">Slow Weight Loss (~0.5lb/week)</option>
+				<option value="1">Maintain Weight (0lb/week)</option>
+				<option value="1.1">Slow Weight Gain (0.5% body weight/month)</option>
+				<option value="1.2">Moderate Weight Gain (1% body weight/month)</option>
+				<option value="1.4">Rapid Weight Gain (2% body weight/month)</option>
 			</select>
 		</div>
-		<section class="text-center text-xl self-center">
-			{macros.protein}
-			{macros.carbs}
-			{macros.fat}
-		</section>
+		{#if macros.protein !== null}
+			<section class="text-center text-xl self-center mt-2">
+				{macros.protein} P
+				{macros.carbs} C
+				{macros.fat} F
+			</section>
+		{/if}
 	</form>
 	<Button on:click|once={calculate} let:calculate><p slot="calculate">{calculate}</p></Button>
 </section>
 
 <style>
 	#form {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		grid-template-rows: 1fr 1fr;
-	}
-
-	input,
-	select {
-		/* text-align: center; */
-		padding-left: 1rem;
+		display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+		/* grid-gap: 1rem; */
+		/* grid-template-columns: 1fr 1fr 1fr;
+		grid-template-rows: 1fr 1fr; */
 	}
 </style>
--- src/routes/calcs/index@calc.svelte
+++ src/routes/calcs/index@calc.svelte
@@ -4,7 +4,7 @@
 
 <h2 class="text-center mt-14 mx-6 py-6 bg-black/50 rounded-lg text-4xl font-bold">Calculator Page</h2>
 
-<section id="calc-section">
+<section id="calc-section" class="xl:grid-cols-2">
 	<CalcCard />
 	<CalcCard />
 	<CalcCard />
@@ -13,7 +13,8 @@
 <style>
     #calc-section {
         display: grid;
-        grid-template-columns: 1fr 1fr;
+        grid-gap: 1rem;
+        /* grid-template-columns: 1fr; */
         /* grid-template-rows: ; */
     }
 </style>
