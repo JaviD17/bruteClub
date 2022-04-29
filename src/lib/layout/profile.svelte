1493e476f225e4537deaa35d81d69749332ad890
Javier Dominguez II <Javi7057@gmail.com>
2022-04-29 11:53:28 -0500

    fixed profile hover feature

--- src/lib/layout/profile.svelte
+++ src/lib/layout/profile.svelte
@@ -1,46 +1,26 @@
 <script>
 	import Button from '$lib/components/button.svelte';
 
-	let hovering = null;
+	let clicked = null;
 	let status = null;
 
-	async function handleStatus(hover = null, currentStatus = null) {
-		if (hover && currentStatus) {
-			hovering = hover; // true when on:mouseenter on pro icon
-			status = currentStatus;
+	let leave = () => {
+		clicked = !clicked;
+	};
 
-			// console.log(hover, currentStatus);
-		} else if (!hover && currentStatus === null) {
-			hovering = hover;
-			status = currentStatus;
-			// console.log(hovering, hover);
-		} else if (hover === null && currentStatus) {
-			hovering = hover;
-			status = currentStatus;
-			console.log(hovering, hover);
-		} else if (hover === null && !currentStatus) {
-			setTimeout(() => {
-				hovering = hover;
-				status = currentStatus;
-			}, 3000);
-		}
+	function handleClick() {
+		clicked = !clicked;
 	}
 </script>
 
-<section
-	on:mouseenter={() => handleStatus(true, true)}
-	on:mouseleave={() => handleStatus(false, ...[,])}
-	id="profileIcon"
-	class="md:grid self-start absolute right-2 hidden"
->
+<section on:click={handleClick} id="profileIcon" class="md:grid self-start absolute right-2 hidden">
 	<Button>
 		<svelte:fragment slot="profile" let:proSvg>{@html proSvg}</svelte:fragment>
 	</Button>
 </section>
-{#if hovering || status}
+{#if clicked}
 	<section
-		on:mouseenter={() => handleStatus(...[,], true)}
-		on:mouseleave={() => handleStatus(...[,], false)}
+		on:mouseleave={leave}
 		id="submenu"
 		class="bg-neutral-900 absolute right-20 top-8 rounded-lg grid"
 	>
